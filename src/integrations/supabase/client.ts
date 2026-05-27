48003995934c5c959f504d1c0854b3baf211010d
Zubair Mohideen <zubairmohideen95@gmail.com>
2026-05-27 23:19:11 +0200

    deploying to website

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
 const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
-const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
+const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
